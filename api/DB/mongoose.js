3d8507eca89405c2127445b1a6d7c915d55a3140
Peterson <44592787+petscaramussi@users.noreply.github.com>
2023-10-30 16:10:33 -0300

    hide connection string

--- api/DB/mongoose.js
+++ api/DB/mongoose.js
@@ -1,9 +1,10 @@
 // this  file will handle connection logic to MongoDB databse
 
 const mongoose = require('mongoose');
+const { yourpass } = require('./config.sample');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://petersonscaramussidasilva:P0Ey6Xyt0DyMZg2q@cluster0.nt0tk1x.mongodb.net/?retryWrites=true&w=majority').then(() => {
+mongoose.connect(`mongodb+srv://petersonscaramussidasilva:${yourpass}@cluster0.nt0tk1x.mongodb.net/?retryWrites=true&w=majority`).then(() => {
     console.log("Connected to MongoDB successfully :)")
 }).catch((e) => {
     console.log("Error while attempting to connect to MongoDB");
