0641c009c33192fdf331959defcbc4f0083952f5
Peterson <44592787+petscaramussi@users.noreply.github.com>
2023-10-30 18:21:34 -0300

    end api requests

--- api/app.js
+++ api/app.js
@@ -42,12 +42,74 @@ app.post('/lists', (req, res) => {
 // PATCH
 app.patch('/lists/:id', (req, res) => {
     // update the specified list (list document with id in the URL) with the new values specified in the JSON body of the request
+    List.findOneAndUpdate({ _id: req.params.id }, {
+        $set: req.body
+    }).then(() => {
+        res.sendStatus(200)
+    })
 })
 
 // DELETE
 app.delete('/lists/:id', (req, res) => {
     // delete the specified list (document with id in the URL)
-})
+    List.findOneAndRemove({
+        _id: req.params.id
+    }).then((removedListDoc) => {
+        res.send(removedListDoc);
+    })
+});
+
+app.get('/lists/:listId/tasks', (req, res) => {
+    // return all taks tha belong to specific list
+    Task.find({
+        _ListId: req.params.listId
+    }).then((tasks) => {
+        res.send(tasks);
+    })
+});
+
+app.get('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOne({
+        _id: req.params.taskId,
+        _ListId: req.params.listId
+    }).then((task) => {
+        res.send(task);
+    })
+});
+
+app.post('/lists/:listId/tasks', (req, res) => {
+    // create a new task in a list specified by listId
+    let newTask = new Task({
+        title: req.body.title,
+        _ListId: req.params.listId
+    });
+    newTask.save().then((newTaskDoc) => {
+        res.send(newTaskDoc)
+    })
+});
+
+app.patch('/lists/:listId/tasks/:taskId', (req, res) => {
+    //update an existing task (specified by taskId)
+    Task.findOneAndUpdate({
+        _id: req.params.taskId,
+        _ListId: req.params.listId
+    },  {
+            $set: req.body
+        }
+    ).then(() => {
+        res.sendStatus(200);
+    })
+});
+
+app.delete('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOneAndRemove({
+        _id: req.params.taskId,
+        _ListId: req.params.listId
+    }).then((removedTaskDoc) => {
+        res.send(removedTaskDoc);
+    })
+});
+
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
