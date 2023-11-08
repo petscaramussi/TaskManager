d48757c9140b04027c7bb5e6f647b6f27979a9ef
Peterson <44592787+petscaramussi@users.noreply.github.com>
2023-11-08 20:58:11 -0300

    Revert "client: login + authentication"
    This reverts commit 5a641e546439c9f9af87b556734aa3ae4f427ca3.

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,6 +1,4 @@
-import { HttpResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +7,4 @@ import { AuthService } from 'src/app/auth.service';
 })
 export class LoginComponent {
 
-  constructor(private authService: AuthService) {}
-
-  onLoginButtonClicked(email: string, password: string) {
-    this.authService.login(email, password).subscribe((res: HttpResponse<any>) => {
-      console.log(res);
-    })
-  }
 }
